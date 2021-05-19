db761a767f32e05543e15a38fa0829fba04ebb65
Brian Smith <brian.bubbam@gmail.com>
2021-05-19 17:26:48 -0500

    commit tree ba1ea4a4b07554ee5e3928e1fdfc0b92258a4e8d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+function superbowlWin(records) {
+    const superbowlWon = records.find(record => record.result === "W")
+    if (superbowlWon) {
+        return superbowlWon.year;
+    } else {
+        return undefined;
+    }
+}
+
+
+//receive one argument that is an array
+//properties: year and result
+//test each Object to see if the result is "W"
+//if so return the year if won
+//if no win return undefined
